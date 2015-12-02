3846e44d524f9a9e1d6bc87ed3abeda493d40896
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-12-01 22:43:53 -0800

    Solve exercise 1 in hw4

--- cis194/spring13/hw4/Solution.hs
+++ cis194/spring13/hw4/Solution.hs
@@ -1,2 +1,12 @@
 fun1' :: [Integer] -> Integer
 fun1' = product . map (\x -> x - 2) . filter even
+
+fun2' :: Integer -> Integer
+fun2' = sum
+        . filter even
+        . takeWhile (/=1)
+        . iterate (\n -> if even n then n `div` 2 else 3 * n + 1)
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+   deriving (Show, Eq)
