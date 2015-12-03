dc696aca6528f9dc7e3abf32280bcaf5ad1f39a9
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-12-02 22:48:24 -0800

    Add more solution to hw4

--- cis194/spring13/hw4/Solution.hs
+++ cis194/spring13/hw4/Solution.hs
@@ -10,3 +10,32 @@ fun2' = sum
 data Tree a = Leaf
             | Node Integer (Tree a) a (Tree a)
    deriving (Show, Eq)
+
+insert :: a -> Tree a -> Tree a
+insert a Leaf = Node 0 Leaf a Leaf
+insert a (Node n l b r) | even n    = Node (n+1) (insert a l) b r
+                        | otherwise = Node (n+1)  l b (insert a r)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
+
+xor :: [Bool] -> Bool
+xor = foldr f False
+    where f True = not
+          f False = id
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr g []
+    where g a b = f a : b
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs)   | x `elem` xs   = rmdups xs
+                | otherwise     = x : rmdups xs
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [ (x,y) | x <- xs, y <- ys ]
+
+sieveOfSundaram :: Integer -> [Integer]
+sieveOfSundaram n = rmdups . map ((\x -> 2*x + 1) . snd) . filter f $ cartProd [1..n] [1..n]
+    where f (x, y) = not ((1 <= x && x <= y && 1<= y) && ((x + y + 2*x*y) <= n))
