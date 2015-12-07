286a4b005050d8e8ced7efd1975ab9a773edefaa
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-12-06 23:00:12 -0800

    Solve more problems from hw5

--- cis194/spring13/hw5/Calc.hs
+++ cis194/spring13/hw5/Calc.hs
@@ -1,17 +1,21 @@
 module Calc where
 import Expr
-import ExprT
+import ExprT as E
 import Parser
+import StackVM as VM
 
 eval :: ExprT -> Integer
 eval (Lit n) = n
-eval (Add e1 e2) = eval e1 + eval e2
-eval (Mul e1 e2) = eval e1 * eval e2
+eval (E.Add e1 e2) = eval e1 + eval e2
+eval (E.Mul e1 e2) = eval e1 * eval e2
 
 evalStr :: String -> Maybe Integer
-evalStr s = case parseExp Lit Add Mul s of
+evalStr s = case parseExp Lit E.Add E.Mul s of
         Just a -> Just (eval a)
         Nothing -> Nothing
 
 testExp :: Expr a => Maybe a
 testExp = parseExp lit add mul "(3 * -4) + 5"
+
+compile :: String -> Maybe Program
+compile = parseExp lit add mul
--- cis194/spring13/hw5/Expr.hs
+++ cis194/spring13/hw5/Expr.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Expr where
-import ExprT
+import ExprT as E
+import StackVM as VM
 
 class Expr a where
     lit :: Integer -> a
@@ -8,8 +11,8 @@ class Expr a where
 
 instance Expr ExprT where
     lit = Lit
-    mul = Mul
-    add = Add
+    mul = E.Mul
+    add = E.Add
 
 instance Expr Integer where
     lit = id
@@ -28,3 +31,13 @@ instance Expr MinMax where
     lit = MinMax
     mul (MinMax x) (MinMax y) = MinMax (min x y)
     add (MinMax x) (MinMax y) = MinMax (max x y)
+
+instance Expr Mod7 where
+    lit n = Mod7 (mod n 7)
+    mul (Mod7 x) (Mod7 y) = Mod7 (mod (x * y) 7)
+    add (Mod7 x) (Mod7 y) = Mod7 (mod (x + y) 7)
+
+instance Expr Program where
+    lit n = [PushI n]
+    mul p1 p2 = p1 ++ p2 ++ [VM.Mul]
+    add p1 p2 = p1 ++ p2 ++ [VM.Add]
